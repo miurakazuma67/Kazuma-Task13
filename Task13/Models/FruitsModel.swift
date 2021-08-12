9d309c0328605ebf4e9b89eae9c15f868e6559cc
akio0911 <akio0911@gmail.com>
2021-08-13 07:54:30 +0900

    feedback

--- Task13/Models/FruitsModel.swift
+++ Task13/Models/FruitsModel.swift
@@ -7,23 +7,11 @@
 
 import Foundation
 
-//protocolかく
-protocol FruitsModelDelegate: AnyObject {
-    func setModel()
-}
-
 final class FruitsModel {
-    weak var delegate: FruitsModelDelegate?
     let fruits: [Fruit] = [
         Fruit(name: "りんご", isChecked: true),
         Fruit(name: "みかん", isChecked: false),
         Fruit(name: "バナナ", isChecked: true),
         Fruit(name: "パイナップル", isChecked: false)
     ]
-    
-    func set(fruits: [Fruit]) {
-        delegate?.setModel()
-    }
 }
-
-
